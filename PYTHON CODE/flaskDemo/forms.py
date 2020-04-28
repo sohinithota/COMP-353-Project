447a1b9c0e879eb0513e54dfbc4e438e8b23f4e7
Nicholas Synovic <nicholas.synovic@gmail.com>
2020-04-28 12:18:52 -0500

    Important changes
    Signed-off-by: Nicholas Synovic <nicholas.synovic@gmail.com>

--- PYTHON CODE/flaskDemo/forms.py
+++ PYTHON CODE/flaskDemo/forms.py
@@ -30,10 +30,8 @@ for row in docid:
 doctorChoices = [(row['docID'],row['docID']) for row in result2]  # change
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
+    accountname = StringField('Account Name',
                            validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
@@ -43,15 +41,10 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
-
+			
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+    accountname = StringField('Account Name',
+                        validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
--- PYTHON CODE/flaskDemo/routes.py
+++ PYTHON CODE/flaskDemo/routes.py
@@ -18,13 +18,13 @@ def login():
 		return redirect(url_for('home'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		user = User.query.filter_by(email=form.email.data).first()
+		user = User.query.filter_by(accountname=form.accountname.data).first()
 		if user and bcrypt.check_password_hash(user.password, form.password.data):
 			login_user(user, remember=form.remember.data)
 			next_page = request.args.get('next')
 			return redirect(next_page) if next_page else redirect(url_for('home'))
 		else:
-			flash('Login Unsuccessful. Please check email and password', 'danger')
+			flash('Login Unsuccessful. Please check account name and password', 'danger')
 	return render_template('login.html', title='Login', form=form)
 
 @app.route("/home")
@@ -58,7 +58,7 @@ def register():
 	form = RegistrationForm()
 	if form.validate_on_submit():
 		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-		user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+		user = User(accountname=form.accountname.data, password=hashed_password)
 		db.session.add(user)
 		db.session.commit()
 		flash('Your account has been created! You are now able to log in', 'success')
